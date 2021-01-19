ebbb9ec08d975fbfbf239ede4217e706224cc5f4
Samyakk123 <samyak.mt@gmail.com>
2021-01-19 17:05:15 -0500

    files with firebase

--- screens/Login.js
+++ screens/Login.js
@@ -1,46 +1,65 @@
-import React from 'react';
-import { StyleSheet, View, Text, Image, TextInput, Button, Alert } from 'react-native';
+import React, { useState } from "react";
+
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TextInput,
+  Button,
+  Alert,
+} from "react-native";
+
+// this.state = {
+//   email: "",
+//   password: "",
+// };
 
 export default function Login({ navigation }) {
-    return (
-        <View style={styles.container}>
-            <Image style={styles.logo}
-                source={require('../Logo/splitify.png')}
-            />
-            <View style={[styles.box, {
-                transform: [{ translateY: -20 }]
-            }]}>
-                <TextInput
-                    placeholder="Your Email"
-                />
-                <TextInput
-                    placeholder="Your Password"
-                />
+  const [email, password] = useState(0);
 
-            </View>
-            <Button
-                title="Login"
-                onPress={() => navigation.push('Main')}
+  loginUser = (email, password) => {};
 
-            />
-            <Button
-                title="Signup"
-                onPress={() => navigation.push('Signup')}
-            />
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require("../Logo/splitify.png")} />
+      <View
+        style={[
+          styles.box,
+          {
+            transform: [{ translateY: -20 }],
+          },
+        ]}
+      >
+        <TextInput
+          placeholder="Your Email"
+          onChangeText={(email) => this.setState({ email })}
+        />
+        <TextInput
+          placeholder="Your Password"
+          onChangeText={(password) => this.setState({ password })}
+        />
+      </View>
+      <Button
+        title="Login"
+        onPress={() => this.loginUser(this.state.email, this.state.password)}
+        onPress={() => navigation.push("Main")}
+      />
+      <Button title="Signup" onPress={() => navigation.push("Signup")} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: '#F5F5F5',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-    },
-    logo: {
-        width: 350,
-        height: 350,
-    },
-})
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#F5F5F5",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  logo: {
+    width: 350,
+    height: 350,
+  },
+});
