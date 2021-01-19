804f20a0e8212b4d8553cb7a2d914ef66f5d252a
Samyakk123 <samyak.mt@gmail.com>
2021-01-19 17:04:43 -0500

    Finished adding with firebase

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Navigation from './routes/stack'
+import React from "react";
+
+import Navigation from "./routes/stack";
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBQ1JEEWRyyAWIicLofN5PbkISbGqSLz8A",
+  authDomain: "splitify-d6ee9.firebaseapp.com",
+  projectId: "splitify-d6ee9",
+  storageBucket: "splitify-d6ee9.appspot.com",
+  messagingSenderId: "722846284602",
+  appId: "1:722846284602:web:f4addad17d7ecd97c2ae03",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
-  return (
-    <Navigation />
-  );
+  return <Navigation />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// firebase.auth().createUserWithEmailAndPassword("testing@gmail.com", "testing2");
