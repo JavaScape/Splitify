def53174c3cb9178143fcfb3c304b5f67f376f37
Aryan Patel <69769265+AryPat@users.noreply.github.com>
2021-02-13 20:37:14 -0500

    Styled Login and Signup Page

--- screens/Login.js
+++ screens/Login.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import * as firebase from "firebase";
+import { Dimensions } from "react-native";
+
+var width = Dimensions.get('window').width - 150; //full width
+var height = Dimensions.get('window').height; //full height
 
 import {
   StyleSheet,
@@ -9,6 +13,8 @@ import {
   TextInput,
   Button,
   Alert,
+  TouchableOpacity,
+
 } from "react-native";
 
 // this.state = {
@@ -42,32 +48,39 @@ export default function Login({ navigation }) {
         style={[
           styles.box,
           {
-            transform: [{ translateY: -20 }],
+            transform: [{ translateY: -70 }],
           },
+          styles.textInputAndButton,
         ]}
       >
         <TextInput
-          placeholder="Your Email"
+          placeholder="Email Address"
+          style={styles.input}
           onChangeText={(text) => setEmail(text)}
         />
         <TextInput
-          placeholder="Your Password"
+          placeholder="Password"
+          style={styles.input}
           onChangeText={(text) => setPassword(text)}
           secureTextEntry={true}
 
-          //   onChangeText={(event) => setPassword(event.target.value)}
+        //   onChangeText={(event) => setPassword(event.target.value)}
         />
-
         {validate.length > 0 && (
-          <Text style={{ color: "red" }}>{validate}</Text>
+          <Text style={{ color: "#cc3300" }}>{validate}</Text>
         )}
+        {validate.length <= 0 && (
+          <Text style={{ color: "#F5F5F5" }}>.</Text>
+        )}
+        <TouchableOpacity onPress={() => loginUser(email, password)} style={styles.appButtonContainer}>
+          <Text style={styles.appButtonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.push("Signup")} style={styles.appButtonContainer}>
+          <Text style={styles.appButtonText}>Signup</Text>
+        </TouchableOpacity>
+
       </View>
-      <Button
-        title="Login"
-        onPress={() => loginUser(email, password)}
-        // onPress={() => navigation.push("Main")}
-      />
-      <Button title="Signup" onPress={() => navigation.push("Signup")} />
+
     </View>
   );
 }
@@ -80,8 +93,44 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
   },
+  textInputAndButton: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    height: 250,
+  },
   logo: {
     width: 350,
     height: 350,
+  },
+  input: {
+    backgroundColor: '#f2f2f2',
+    borderColor: '#a6a6a6',
+    borderRadius: 10,
+    borderWidth: 1,
+    padding: 2,
+    paddingLeft: 8,
+
+    alignSelf: 'center',
+    width: width,
+
+
+  },
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: "#ff8080",
+    borderRadius: 10,
+    paddingVertical: 8,
+    paddingHorizontal: 8,
+
+  },
+  appButtonText: {
+    fontSize: 15,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase",
+    width: 100,
+    textAlign: 'center', // <-- the magic
   },
 });
--- screens/Signup.js
+++ screens/Signup.js
@@ -7,8 +7,14 @@ import {
   TextInput,
   Button,
   Alert,
+  TouchableOpacity,
 } from "react-native";
 import * as firebase from "firebase";
+
+import { Dimensions } from "react-native";
+
+var width = Dimensions.get('window').width - 150; //full width
+var height = Dimensions.get('window').height; //full height
 
 export default function Signup({ navigation }) {
   const [name, setName] = useState("");
@@ -77,36 +83,47 @@ export default function Signup({ navigation }) {
         style={[
           styles.box,
           {
-            transform: [{ translateY: -20 }],
+            transform: [{ translateY: -70 }],
           },
+          styles.textInputAndButton,
         ]}
       >
-        <TextInput placeholder="Name" onChangeText={(text) => setName(text)} />
-        {nameErr.length > 0 && <Text style={{ color: "red" }}>{nameErr}</Text>}
+        <TextInput placeholder="Username" style={styles.input} onChangeText={(text) => setName(text)} />
+        {nameErr.length > 0 && <Text style={{ color: "#cc3300" }}>{nameErr}</Text>}
+        {nameErr.length <= 0 && <Text style={{ color: "#F5F5F5" }}>.</Text>}
 
         <TextInput
-          placeholder="Your Email"
+          placeholder="Email Address"
+          style={styles.input}
           onChangeText={(text) => setEmail(text)}
         />
         {emailErr.length > 0 && (
-          <Text style={{ color: "red" }}>{emailErr}</Text>
+          <Text style={{ color: "#cc3300" }}>{emailErr}</Text>
         )}
+        {emailErr.length <= 0 && <Text style={{ color: "#F5F5F5" }}>.</Text>}
 
         <TextInput
-          placeholder="Your Password"
+          placeholder="Password"
+          style={styles.input}
           onChangeText={(text) => setPassword(text)}
           secureTextEntry={true}
         />
         {passwordErr.length > 0 && (
-          <Text style={{ color: "red" }}>{passwordErr}</Text>
+          <Text style={{ color: "#cc3300" }}>{passwordErr}</Text>
         )}
-      </View>
+        {passwordErr.length <= 0 && (
+          <Text style={{ color: "#F5F5F5" }}>.</Text>
+        )}
+        <TouchableOpacity onPress={() => signupUser()} style={styles.appButtonContainer}>
+          <Text style={styles.appButtonText}>Signup</Text>
+        </TouchableOpacity>
 
-      <Button title="Signup" onPress={() => signupUser()} />
-      <Button
-        title="Already have an account? Click here."
-        onPress={() => navigation.push("Login")}
-      />
+        <TouchableOpacity onPress={() => navigation.push("Login")} style={styles.appButtonContainer}>
+          <Text style={styles.appButtonText}>Login</Text>
+        </TouchableOpacity>
+
+
+      </View>
     </View>
   );
 }
@@ -119,8 +136,45 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
   },
+  textInputAndButton: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    height: 265,
+  },
   logo: {
     width: 350,
     height: 350,
+  },
+  input: {
+    backgroundColor: '#f2f2f2',
+    borderColor: '#a6a6a6',
+    borderRadius: 10,
+    borderWidth: 1,
+    padding: 2,
+    paddingLeft: 8,
+
+    alignSelf: 'center',
+    width: width,
+
+
+  },
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: "#ff8080",
+    borderRadius: 10,
+    paddingVertical: 8,
+    paddingHorizontal: 8,
+    margin: 6,
+
+  },
+  appButtonText: {
+    fontSize: 15,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase",
+    width: 100,
+    textAlign: 'center', // <-- the magic
   },
 });
