c8d59e89373614ccb352f93b7a6d5d05c2bb6fc9
Samyakk123 <samyak.mehta@mail.utoronto.ca>
2021-02-14 20:34:36 -0500

    Modified firebaseCommand file

--- components/firebaseCommands.js
+++ components/firebaseCommands.js
@@ -1,4 +1,32 @@
 import * as firebase from "firebase";
 import "firebase/firestore";
 
+var firebaseConfig = {
+  apiKey: "AIzaSyBQ1JEEWRyyAWIicLofN5PbkISbGqSLz8A",
+  authDomain: "splitify-d6ee9.firebaseapp.com",
+  projectId: "splitify-d6ee9",
+  storageBucket: "splitify-d6ee9.appspot.com",
+  messagingSenderId: "722846284602",
+  appId: "1:722846284602:web:f4addad17d7ecd97c2ae03",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
 const database = firebase.firestore();
+var storageRef = firebase.storage().ref();
+
+const uploadImage = async (image, userId) => {
+  const response = await fetch(image);
+  const blob = await response.blob();
+
+  var setImage = storageRef.child("images/" + userId);
+  return setImage.put(blob).catch((e) => {
+    console.log(e);
+  });
+};
+
+export { uploadImage };
