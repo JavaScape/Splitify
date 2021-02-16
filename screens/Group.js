709acb54ada1c98c4aeb7bbc5717441abb2651ca
Aryan Patel <69769265+AryPat@users.noreply.github.com>
2021-02-15 19:06:58 -0500

    Fixed Height Issue
    Background now covers all of the screen and scrolling is enabled

--- screens/Group.js
+++ screens/Group.js
@@ -1,25 +1,31 @@
 import React from 'react';
 
-import { StyleSheet, View, Text, TouchableOpacity, Dimensions } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Dimensions, ScrollView } from 'react-native';
 var width = Dimensions.get("window").width - 80; //full width
 var height = Dimensions.get("window").height; //full height
 
 export default function Group({ navigation }) {
     return (
-        <View style={styles.container}>
-            <TouchableOpacity onPress={() => navigation.push("CreateGroup")} style={styles.appButtonContainer}>
-                <Text style={styles.appButtonText}>Create Group</Text>
-            </TouchableOpacity>
-        </View>
+        <ScrollView contentContainerStyle={styles.outside}>
+            <View style={styles.container}>
+                <TouchableOpacity onPress={() => navigation.push("CreateGroup")} style={styles.appButtonContainer}>
+                    <Text style={styles.appButtonText}>Create Group</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
 
     )
 }
 
 const styles = StyleSheet.create({
+    outside: {
+        height: "100%",
+        backgroundColor: "#ecf9f2",
+    },
     container: {
         padding: 24,
         backgroundColor: "#ecf9f2",
-        height: height
+        height: "auto"
     },
     appButtonContainer: {
         elevation: 8,
